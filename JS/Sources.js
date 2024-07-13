2d7f9bf48ec696fc5cf6e4d163b2dac263177aae
Nguyen Dang Khoa <it.khoand@gmail.com>
2024-07-14 02:43:56 +0700

    add sources.js

---
+++ JS/Sources.js
@@ -0,0 +1,38 @@
+var Color = {
+    while: "255,255,255",
+    Black: "170,182,155",
+    Move: "246,205,97"
+}
+var Black = {
+    Tot: 'QuanCo/Tot_Den.png',
+    Tuong: 'QuanCo/Tuong_Den.png',
+    Ma: 'QuanCo/Ma_Den.png',
+    Xe: 'QuanCo/Xe_Den.png',
+    Hau: 'QuanCo/Hau_Den.png',
+    Vua: 'QuanCo/Vua_Den.png',
+    Rong: 'QuanCo/Rong.png',
+    //Name
+    Tot_Name: 'Tot_Den',
+    Tuong_Name: 'Tuong_Den',
+    Ma_Name: 'Ma_Den',
+    Xe_Name: 'Xe_Den',
+    Hau_Name: 'Hau_Den',
+    Vua_Name: 'Vua_Den'
+}
+
+var Red = {
+    Tot: 'QuanCo/Tot_Do.png',
+    Tuong: 'QuanCo/Tuong_Do.png',
+    Ma: 'QuanCo/Ma_Do.png',
+    Xe: 'QuanCo/Xe_Do.png',
+    Hau: 'QuanCo/Hau_Do.png',
+    Vua: 'QuanCo/Vua_Do.png',
+    Rong: 'QuanCo/Rong.png',
+    //Name
+    Tot_Name: 'Tot_Do',
+    Tuong_Name: 'Tuong_Do',
+    Ma_Name: 'Ma_Do',
+    Xe_Name: 'Xe_Do',
+    Hau_Name: 'Hau_Do',
+    Vua_Name: 'Vua_Do'
+}
